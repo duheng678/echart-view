6da3b9fdb885dcacdbd07a24fc5193943bd97df0
moondu <duheng678@163.com>
2022-06-08 10:09:00 +0800

    vue-echart and v-chart

--- src/main.js
+++ src/main.js
@@ -2,9 +2,15 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import './plugins/element.js';
+import './plugins/v-chart';
 import * as Echarts from 'echarts';
+
+// import VueECharts from 'vue-echarts';
+// Vue.component('v-chart', VueECharts);
+
 Vue.config.productionTip = false;
 Vue.prototype.$echarts = Echarts;
+
 new Vue({
     router,
     render: h => h(App),
